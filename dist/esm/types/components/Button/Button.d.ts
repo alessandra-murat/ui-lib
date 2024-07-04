fc0d9b313421e5f7c383fff9e57e8985ef151bc3
Your NameAlessandra <alessandra.murat@jsmx.com.br>
2024-07-04 15:53:54 -0300

    Remover dist do histórico do Git e adicionar ao .gitignore

--- dist/esm/types/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import React, { InputHTMLAttributes } from "react";
-export interface OwnProps extends InputHTMLAttributes<HTMLButtonElement> {
-    label: string;
-}
-declare const ButtonWrapper: (props: OwnProps) => React.JSX.Element;
-export default ButtonWrapper;
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { InputHTMLAttributes } from 'react';
-
-interface OwnProps extends InputHTMLAttributes<HTMLButtonElement> {
-    label: string;
-}
-declare const ButtonWrapper: (props: OwnProps) => React.JSX.Element;
-
-export { ButtonWrapper as Button };
